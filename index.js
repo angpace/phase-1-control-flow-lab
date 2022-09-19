db6620faf3630e16d009533e9a8f266e76f82969
Angela Pace <angpace13@gmail.com>
2022-09-19 13:45:01 -0400

    commit tree c7c2d21df74b8e7282e3be0e58e023e88145e490

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Sudo code- gives customer a free sample if the ride is less than or equal to 400 feet
+  // if 
+  // less than or equal 
+  //400
+  let x = feet 
+  if (x <= 400){
+    return ("This one is on me!")
+  }
+
 }
+
+scuberGreetingForFeet(199)
 
 function ternaryCheckCity(){
   // Write your code here!
